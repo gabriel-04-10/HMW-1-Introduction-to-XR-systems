f3a76240611134474c77d7c9e36aec8b7ba3c4a7
gabriel-04-10 <156429999+gabriel-04-10@users.noreply.github.com>
2024-01-27 18:42:12 +0200

    breakout restant

--- HMW-1-Unity/Assets/Scenes/Light switch.cs
+++ HMW-1-Unity/Assets/Scenes/Light switch.cs
@@ -1,17 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
-public class LightSwitch : MonoBehaviour
+public class ChangeLightColor : MonoBehaviour
 {
-    public new Light light;
-    void Start() 
-    {
-        light = GetComponent<Light>();
-    } 
+    public Light targetLight;
+    public float colorChangeSpeed = 1.0f;
+
     void Update()
     {
-        if (Input.GetKeyDown("tab"))
-        {light.color = Color.blue;}
+        if (HasOculusXRSubsystem())
+        {
+            string loadedDeviceName = XRSettings.loadedDeviceName;
+            if (loadedDeviceName.Contains("Oculus"))
+            {
+                float triggerValue = Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger");
+                Color newColor = Color.Lerp(Color.red, Color.blue, triggerValue);
+                targetLight.color = Color.Lerp(targetLight.color, newColor, Time.deltaTime * colorChangeSpeed);
+            }
+        }
+    }
+    bool HasOculusXRSubsystem()
+    {
+        var subsystemDescriptors = new List<XRDisplaySubsystemDescriptor>();
+        SubsystemManager.GetSubsystemDescriptors(subsystemDescriptors);
+
+        foreach (var descriptor in subsystemDescriptors)
+        {
+            if (descriptor.id.Contains("Oculus"))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
+
